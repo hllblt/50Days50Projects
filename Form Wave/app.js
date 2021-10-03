6ffcdffd190400846004d08de064107fdcde0388
Halil <halill.bulutt.1@gmail.com>
2021-10-03 16:56:53 +0200

    created form wave page

---
+++ Form Wave/app.js
@@ -0,0 +1,11 @@
+// const email = document.getElementById("mail");
+// const password = document.getElementById("password");
+// const label1 = document.getElementsByTagName("label")[0];
+// const label2 = document.getElementsByTagName("label")[1];
+
+// email.addEventListener("click", () => {
+//   label1.style.color = "red";
+// });
+// password.addEventListener("click", () => {
+//   label2.style.color = "green";
+// });
